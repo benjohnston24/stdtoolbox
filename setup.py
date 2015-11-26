a1e98578f74420fa6d67cc280de4017ac8790ebc
Ben Johnston <bjohnston@neomailbox.net>
2015-11-26 20:24:05 +1100

    backup

--- setup.py
+++ setup.py
@@ -1,4 +1,40 @@
-stdtoolbox
-==========
+#! /usr/bin/env python
+"""!
+-----------------------------------------------------------------------------
+File Name : setup.py
 
-Standard Python Toolbox
+Purpose:
+
+Created: 04-Jun-2015 23:18:18 AEST
+-----------------------------------------------------------------------------
+Revision History
+
+
+
+-----------------------------------------------------------------------------
+S.D.G
+"""
+__author__ = 'Ben Johnston'
+__revision__ = '0.1'
+__date__ = '04-Jun-2015 23:18:18 AEST'
+__license__ = 'MPL v2.0'
+
+## LICENSE DETAILS############################################################
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+##IMPORTS#####################################################################
+from distutils.core import setup
+##############################################################################
+setup(
+    name='stdtoolbox',
+    description='',
+    url='',
+    author='',
+    author_email='',
+    version=__revision__,
+    packages=['stdtoolbox'],
+    license='http://mozilla.org/MPL/2.0',
+    long_description='')
+
